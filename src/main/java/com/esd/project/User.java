12fc581c6156573c166fe51fae96af9b16e938ba
Shivamtiwari6393 <shivamtiwari6223@gmail.com>
2023-10-10 11:03:13 +0530

    registration working - user,usercontroller,userrepository,securityconfig

---
+++ src/main/java/com/esd/project/User.java
@@ -0,0 +1,81 @@
+package com.esd.project;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+@Entity
+public class User {
+    // ID-----------ID-------------------ID-------------------------------------ID-------------------
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // USERNAME-------------- USERNAME--------- USERNAME---------- USERNAME-----
+    // USERNAME---------
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // PASSWORD------------PASSWORD-------------------PASSWORD-------------------PASSWORD----------PASSWORD-----
+
+    @Column(nullable = false)
+    private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public CharSequence getPassword() {
+        return password;
+    }
+
+    // CONFIRMPASSWORD---------CONFIRMPASSWORD------------CONFIRMPASSWORD------CONFIRMPASSWORD---------CONFIRMPASSWORD-----------
+
+    @Transient
+    private String confirmPassword;
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    // USER_CREATED_AT----------USER_CREATED_AT--------------USER_CREATED_AT-------------USER_CREATED_AT-----------USER_CREATED_AT---------
+
+    @CreationTimestamp
+    @Column(name = "user_created_at")
+    private LocalDateTime userCreatedAt;
+
+    // STATUS---------STATUS--------------STATUS-----------STATUS-------------STATUS-----------------STATUS----------STATUS---------
+    @Column(nullable = false, columnDefinition = "tinyint default 1")
+    private int status = 1;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
+// -----------------------------------------------------------------------------------------------------------------------------------
---
+++ src/main/java/com/esd/project/UserController.java
@@ -0,0 +1,53 @@
+package com.esd.project;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/registration")
+    public String registrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUser(User user) {
+        // Check if the username already exists in the database
+        User existingUser = userRepository.findByUsername(user.getUsername());
+
+        if (existingUser != null) {
+            return "redirect:/user/registration?error=username_exists";
+        }
+        String confirmpassword = user.getConfirmPassword();
+        System.out.println(confirmpassword);
+        System.out.println(user.getPassword());
+        System.out.println(confirmpassword.equals(user.getPassword()));
+        if (confirmpassword.equals(user.getPassword())) {
+            String hashedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(hashedPassword);
+
+            userRepository.save(user);
+            return "redirect:/user/registration?success";
+        } else {
+            return "redirect:/user/registration?passworderror=password not matching";
+        }
+
+    }
+}
